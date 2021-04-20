86bf464405d94b83b48bd4ed06cc1545b17ba826
Masaki Yoshiiwa <yopinoji@gmail.com>
2021-04-20 15:17:57 +0900

    fix

--- src/components/atoms/Background/Background.tsx
+++ src/components/atoms/Background/Background.tsx
@@ -3,7 +3,10 @@ import BGAnimation from "./BGAnimation";
 const Background: React.FC = ({ children }) => {
   return (
     <>
-      <BGAnimation />
+      <div className="fixed w-full h-full bg-white dark:bg-black transition duration-500 ease-in-out">
+        <div className="absolute opacity-50 -bottom-3/4 w-6/12 h-full animate-float bg-gradient-to-r from-gray-700 dark:from-gray-300" />
+        <BGAnimation />
+      </div>
       <div className="relative">{children}</div>
     </>
   );
